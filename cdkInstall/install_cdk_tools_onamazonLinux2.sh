ffd6f03b0f79176a52720fc03fdc6fc60ae310c7
Pat@maendeleolab <50278702+maendeleolab@users.noreply.github.com>
2023-04-19 21:38:46 -0400

    Update install_cdk_tools_onamazonLinux2.sh

--- cdkInstall/install_cdk_tools_onamazonLinux2.sh
+++ cdkInstall/install_cdk_tools_onamazonLinux2.sh
@@ -2,42 +2,42 @@
 
 # NOTES: This script works well on Amazon Linux2
 
-#update latest packages
+echo "update latest packages"
 sudo yum update && sudo yum upgrade -y
 
-#install nvm (node version manager)
+echo "install nvm (node version manager)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-#activate nvm
+echo "activate nvm"
 . ~/.nvm/nvm.sh
 
-#install nvm version 16
+echo "install nvm version 16"
 #find out why here https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
-nvm install 16
+sudo nvm install 16
 
-#test install is correct
+echo "testing if install is correct"
 node -e "console.log('Running Node.js ' + process.version)"
 
-#Install NodeJs and npm
+echo "Install NodeJs and npm"
 sudo yum install -y nodejs
 
-#output version
+echo "output version"
 node -v
 npm -v
 
-#install the latest version of npm
+echo "install the latest version of npm"
 sudo npm install -g npm@latest
 
 #list npm versions available
 #npm view webpack versions --json
 
-#install CDK toolkit
+echo "install CDK toolkit"
 sudo npm install -g aws-cdk 
-#show CDK version
+echo "show CDK version"
 cdk --version
 
-#make sure the latest pip is installed
-pip install pip --upgrade
+echo "make sure the latest pip is installed"
+pip3 install pip --upgrade
 
 #create a directory for CDK projects
 #mkdir $1
