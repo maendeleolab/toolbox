bea66740d98d992b11466a0bece3bc7dbb4883c1
Pat@maendeleolab <50278702+maendeleolab@users.noreply.github.com>
2023-04-24 12:27:02 -0400

    Update prerequisites.sh

--- networkingTools/prerequisites.sh
+++ networkingTools/prerequisites.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 #chaining commands 
 sudo apt update && sudo apt upgrade -y && sudo apt install dnsutils traceroute mtr iperf hping3 fping inet-tools -y 
-=======
 #installing network utilities needed to perform network performance tests 
 sudo apt update && sudo apt upgrade -y && sudo apt install dnsutils traceroute mtr iperf hping3 fping netcat -y 
->>>>>>> 4d71b2458c20bdf08777f715aca0a738392451bc
