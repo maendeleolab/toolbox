09a725632fa45a220368bd04ddb46539e3e3821d
Pat@maendeleolab <50278702+maendeleolab@users.noreply.github.com>
2023-08-20 22:30:41 -0400

    Create py_webserver.sh
    added file to automate web server test

---
+++ networkingTools/py_webserver.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Installs tmux, if you don't have it yet
+sudo apt-get update
+sudo apt-get install tmux -y
+chmod +x 400 py_webserver.sh
+# Create a new tmux session named "test_web"
+tmux new-session -d -s test_web
+
+# Run a command inside the "test_web" session.
+# Remember to change the target region to the one you are using.
+# The command uses python http module to start a web server listening on TCP port number 5454.
+tmux send-keys -t test_web 'sudo python3 -m http.server 5454' C-m
