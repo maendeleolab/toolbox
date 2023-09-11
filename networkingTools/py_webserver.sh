0256298be663c166577eff99002823e23357dd1d
Pat@maendeleolab <50278702+maendeleolab@users.noreply.github.com>
2023-09-11 17:13:40 -0400

    Update py_webserver.sh

--- networkingTools/py_webserver.sh
+++ networkingTools/py_webserver.sh
@@ -10,4 +10,4 @@ tmux new-session -d -s test_web
 # Run a command inside the "test_web" session.
 # Remember to change the target region to the one you are using.
 # The command uses python http module to start a web server listening on TCP port number.
-tmux send-keys -t test_web 'sudo python3 -m http.server 80' C-m
+tmux send-keys -t test_web 'sudo python3 -m http.server 443' C-m
