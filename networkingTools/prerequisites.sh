97bee2e0a8ac3c5cebed01b970985e02b782cf64
Pat@maendeleolab <50278702+maendeleolab@users.noreply.github.com>
2024-01-12 12:43:20 -0500

    Update prerequisites.sh

--- networkingTools/prerequisites.sh
+++ networkingTools/prerequisites.sh
@@ -2,7 +2,6 @@
 
 ##installing network utilities needed to perform network performance tests
 sudo apt update
-sudo apt install dnsutils -y
 sudo apt install traceroute -y
 sudo apt install mtr -y
 sudo apt install iperf -y
